71a6f7b3a58c53b5596727677f1fe1dad817aa17
Atlinkiha <amirmohammad.rezaie9731@gmail.com>
2016-04-17 17:11:58 +0430

    this is my first commit

---
+++ app/src/main/java/android/example/ir/example/Help.java
@@ -0,0 +1,14 @@
+package android.example.ir.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Help extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
